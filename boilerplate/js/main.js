1fa8cdb7ceba78c049b535d8b1e3e6715907ab05
Rakeshbaghel021 <rkrkumar021@gmail.com>
2019-09-01 21:49:32 +0530

    update index.html

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,22 @@
-// You js goes here
+var quotes=[{
+	         quote:"yayayya",
+	         author:"jkghjhfg"
+           },
+           {
+           	quote:"rrrrrrrr",
+           	author:"jk"
+           },
+           {
+           	quote:"dhjh",
+           	author:"kjkhj"
+           }
+           ]
+function changequotes() {
+	var num=Math.floor(Math.random()*quote.length);
+	var h1=document.getElementById("heading");
+	var h5=document.getElementById("para");
+
+	h1.innerText=quotes[num].quote;
+	h5.innerText=quotes[num].author;
+}
+document.getElementById("click").addEventListener("click",changequotes);
